855e90c0997c2a2958a912b85402a5553d448485
Kostadin Seymenski <seimenski@icloud.com>
2021-03-08 17:23:57 +0200

    Update navbar title

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <router-link class="navbar-brand" to="/">MEVN-Auth</router-link>
+    <router-link class="navbar-brand" to="/">MEVN</router-link>
     <button
       class="navbar-toggler"
       type="button"
