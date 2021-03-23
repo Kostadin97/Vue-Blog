d4b5bb37cff35b876ebba8e6d8f73feea4d381a3
Kostadin Seymenski <seimenski@icloud.com>
2021-03-24 00:38:54 +0200

    Update Login.vue

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -56,7 +56,7 @@ export default {
       };
       this.login(user).then((res) => {
         if (res.data.success) {
-          this.$router.push("/profile");
+          this.$router.push("/");
         }
       });
     },
