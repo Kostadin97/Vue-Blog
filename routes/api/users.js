2909f4c7b1cc911b3a768855097de4ee1c0d32b5
Kostadin Seymenski <seimenski@icloud.com>
2021-04-10 11:59:21 +0300

    Register API updated

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,36 +12,44 @@ const User = require("../../model/User");
  * @access Public
  */
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   let { name, username, email, password, confirm_password } = req.body;
   let hasError = Boolean;
 
-  if (password !== confirm_password) {
-    return res.status(400).json({
-      msg: "Password do not match.",
+  try {
+    if (
+      name === "" ||
+      username === "" ||
+      email === "" ||
+      password === "" ||
+      confirm_password === ""
+    ) {
+      return res.status(404).json({
+        msg: "Please fill all the inputs.",
+        success: false,
+      });
+    }
+    if (password !== confirm_password) {
+      return res.status(400).json({
+        msg: "Password do not match.",
+      });
+    }
+
+    await User.findOne({ username: username }).then((user) => {
+      if (user) {
+        return res.status(400).json({
+          msg: "Username is already taken.",
+        });
+      }
     });
-  }
 
-  // Check for the unique Username
-  User.findOne({ username: username }).then((user) => {
-    if (user) {
-      return res.status(400).json({
-        msg: "Username is already taken.",
-      });
-    }
-  });
-
-  // Check for the unique Email
-  User.findOne({ email: email }).then((user) => {
-    if (user) {
-      return res.status(400).json({
-        msg: "Email is already registered. Did you forgot your password?",
-      });
-    }
-  });
-
-  // The data is valid and now we can register the user
-
+    await User.findOne({ email: email }).then((user) => {
+      if (user) {
+        return res.status(400).json({
+          msg: "Email is already registered. Did you forgot your password?",
+        });
+      }
+    });
     let newUser = new User({
       name,
       username,
@@ -50,8 +58,7 @@ router.post("/register", (req, res) => {
       savedPosts: [],
     });
 
-    // Hash the password
-    bcrypt.genSalt(10, (err, salt) => {
+    await bcrypt.genSalt(10, (err, salt) => {
       bcrypt.hash(newUser.password, salt, (err, hash) => {
         if (err) throw err;
         newUser.password = hash;
@@ -63,7 +70,9 @@ router.post("/register", (req, res) => {
         });
       });
     });
-
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 /**
